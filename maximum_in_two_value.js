282ce18da88d679c25136685329932787e0eead4
Reena Kumari <reena20@navgurukul.org>
2021-07-01 14:21:33 +0530

    first commot

--- maximum_in_two_value.js
+++ maximum_in_two_value.js
@@ -1,3 +1,14 @@
 // Take two int values from user and print greatest among them.
 
 const input=require('readline-sync');
+let num1=Number(input.question("Enter the first number: "));
+let num2=Number(input.question("Enter the second number: "))
+if (num1>num2){
+    console.log("num1 is greatest");
+}
+else if (num1<num2){
+    console.log("num2 is greatest");
+}
+else{
+    console.log("both are equal");
+}
